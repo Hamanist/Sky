bd422ccfe7b05d86e4319c4f8632216751ac7d99
Hamanist <hamanist@gmail.com>
2023-10-17 13:10:26 +0300

    added docstrings and a new feature

--- str_func.py
+++ str_func.py
@@ -1,2 +1,14 @@
 def capital_letters(words: str):
+    """
+    :param words: поступает строка
+    :return: все буквы в верхнем регистре
+    """
     return words.upper()
+
+
+def title_letters(words: str):
+    """
+    :param words: поступает строка
+    :return: все слова начинаются с верхнего регистра
+    """
+    return words.title()
